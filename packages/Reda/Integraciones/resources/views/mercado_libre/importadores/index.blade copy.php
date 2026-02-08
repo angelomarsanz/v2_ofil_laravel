3517303db356a103f2ad17a4b7cb574d560b9a65
angelomarsanz <angelomarsanz@gmail.com>
2026-02-08 17:10:21 +0000

    202602081310

--- packages/Reda/Integraciones/resources/views/mercado_libre/importadores/index.blade copy.php
+++
@@ -1,9 +0,0 @@
-@extends('user.layout')
-
-@section('contenido_del_plugin')
-    <div id='indexImportadores'></div>
-    <br /><br /><br />
-    <div class="alert alert-info">
-        Módulo: {{ config('integracion.module_version') }}
-    </div>
-@endsection
